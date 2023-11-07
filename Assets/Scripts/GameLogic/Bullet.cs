340f7767ffa553f7b568404876ee21c9ef2acca8
Alexander <102901721+Labu1ay@users.noreply.github.com>
2023-11-07 22:43:50 +0300

    Fix SlowZoneGravity

--- Assets/Scripts/GameLogic/Bullet.cs
+++ Assets/Scripts/GameLogic/Bullet.cs
@@ -4,13 +4,12 @@ namespace GameLogic {
     [RequireComponent(typeof(Rigidbody2D))]
     public class Bullet : MonoBehaviour {
         [SerializeField] private Rigidbody2D _rigidbody;
-
         private void OnValidate() => _rigidbody ??= GetComponent<Rigidbody2D>();
 
         public void Init(Vector3 direction, float speed) {
             _rigidbody.velocity = direction * speed;
         }
-    
+
         public void Destroy() => gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/GameLogic/SlowdownZone.cs
+++ Assets/Scripts/GameLogic/SlowdownZone.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace GameLogic {
     public class SlowdownZone : MonoBehaviour {
         [SerializeField] private float _slowdownValue = 10f;
+        private float _squareSlowdownValue;
+        
+        private void Start() => _squareSlowdownValue = Mathf.Pow(_slowdownValue, 2);
+
         private void OnTriggerEnter2D(Collider2D other) {
             other.TryGetComponent(out Rigidbody2D rigidbody);
             if (rigidbody) {
                 rigidbody.velocity /= _slowdownValue;
-                rigidbody.gravityScale /= _slowdownValue;
-                rigidbody.drag *= _slowdownValue;
+                rigidbody.gravityScale /= _squareSlowdownValue;
             }
         }
 
@@ -16,8 +20,7 @@ namespace GameLogic {
             other.TryGetComponent(out Rigidbody2D rigidbody);
             if (rigidbody) {
                 rigidbody.velocity *= _slowdownValue;
-                rigidbody.gravityScale *= _slowdownValue;
-                rigidbody.drag /= _slowdownValue;
+                rigidbody.gravityScale *= _squareSlowdownValue;
             }
         }
     }
